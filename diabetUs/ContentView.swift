274cd361740a87f9d6f8035c1ca65d654c4e32e7
Nedine <nedinerafid@gmail.com>
2021-11-27 14:12:24 +0200

    Initial Commit

---
+++ diabetUs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  diabetUs
+//
+//  Created by Yasmine Rafid on 11/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ diabetUs/diabetUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  diabetUsApp.swift
+//  diabetUs
+//
+//  Created by Yasmine Rafid on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct diabetUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
